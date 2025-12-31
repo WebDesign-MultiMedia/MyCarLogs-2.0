2d7d8958e1685be7f94923f11ebbf1d125f7516c
WebDesign-MultiMedia <salasjulio386@gmail.com>
2025-12-30 20:52:24 -0500

    new Carlog page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: { 
+        'logoClr': '#20b2aa',
+        'logoClrBlue': '#4485d1',
+        'spreadsheetClr': '#0ab677ff',
+        'txtWht': '#f5f5f5',
+      },
+    },
+  },
+  plugins: [],
+}
+
